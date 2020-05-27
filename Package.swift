a317a1cb96d78c1a0f970c365be558a6986c1e5a
Jeremy Greenwood <jeremy@mobelux.com>
2020-05-27 09:18:23 -0400

    package name update

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "swift-log-data-dog",
+    name: "swift-log-datadog",
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
