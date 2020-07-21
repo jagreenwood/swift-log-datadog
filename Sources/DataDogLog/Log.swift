1a9c7dbe41a807e4e9de07aff900b595b169aa77
iq3 <iq3@addli.co.jp>
2020-07-21 12:43:32 +0900

    Added status to property.

--- Sources/DataDogLog/Log.swift
+++ Sources/DataDogLog/Log.swift
@@ -1,10 +1,12 @@
 import Logging
 
+/// https://docs.datadoghq.com/logs/log_collection/?tab=http#reserved-attributes
 struct Log: Encodable {
     let ddsource: String
     let ddtags: String
     let hostname: String
     let message: String
+    let status: String
 }
 
 struct Message: CustomStringConvertible {
