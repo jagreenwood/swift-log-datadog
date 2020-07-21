94b58ad75edd66e7424acfb2abf208247575201d
iq3 <iq3@addli.co.jp>
2020-07-21 13:12:19 +0900

    Add comment

--- Sources/DataDogLog/Log.swift
+++ Sources/DataDogLog/Log.swift
@@ -1,11 +1,18 @@
 import Logging
 
-/// https://docs.datadoghq.com/logs/log_collection/?tab=http#reserved-attributes
+/// Attribute for Datadog Logs
+///
+/// See https://docs.datadoghq.com/logs/log_collection/#reserved-attributes
 struct Log: Encodable {
     let ddsource: String
     let ddtags: String
     let hostname: String
     let message: String
+    
+    /// Log Status
+    ///
+    /// Logger.Level.trace will be sorted into Datadog as .debug.
+    /// See https://docs.datadoghq.com/logs/processing/processors/#log-status-remapper for details.
     let status: String
 }
 
