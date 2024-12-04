1b9b6a2883b84c04e91220af188fb690a36673d3
Jeremy Greenwood <jagreenwood@gmail.com>
2024-12-04 12:29:27 -0500

    update timestamp handling

--- Sources/DataDogLog/Internal/Extensions/String+Timestamp.swift
+++ Sources/DataDogLog/Internal/Extensions/String+Timestamp.swift
@@ -4,8 +4,11 @@ extension String {
     static var timestamp: String {
         var buffer = [Int8](repeating: 0, count: 255)
         var timestamp = time(nil)
-        let localTime = localtime(&timestamp)
+        guard let localTime = localtime(&timestamp) else {
+            return "<unknown>"
+        }
         strftime(&buffer, buffer.count, "%Y-%m-%dT%H:%M:%S%z", localTime)
+
         return buffer.withUnsafeBufferPointer {
             $0.withMemoryRebound(to: CChar.self) {
                 String(cString: $0.baseAddress!)
